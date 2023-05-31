112878f6343f3548e11ef502353d1e20f960c877
ktalary <63938030+ktalary@users.noreply.github.com>
2023-05-31 15:54:22 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://13.212.1.10/CollegeMgmt_RDS/';
+$config['base_url'] = '13.233.92.97/CollegeMgmt_RDS/';
 
 /*
 |--------------------------------------------------------------------------
